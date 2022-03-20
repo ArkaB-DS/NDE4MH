5b4241f11c8f107668a43ff81fa47db9f5f347e1
Arkajyoti Bhattacharjee <arkastat98@gmail.com>
2022-03-20 13:08:32 +0530

    R code and plot showing individual Gaussian kernels

---
+++ kdeContributions.R
@@ -0,0 +1,21 @@
+## data
+x <- c(0, 1, 1.1, 1.5, 1.9, 2.8, 2.9, 3.5)
+n <- length(x)
+
+## create a grid
+xgrid <- seq(from = min(x) - 1, to = max(x) + 1, by = 0.01)
+
+## window width
+h <- 0.4
+
+# create bumps with width h and shape "Gaussian"
+bumps <- sapply(x, function(k) dnorm((xgrid - k)/h)/(n * h))
+
+## Fig 7.2: Kernel estimate showing the contributions of Gaussian kernels evaluated
+## for the individual observations with bandwidth h = 0.4.
+pdf("kdectrb.pdf", height = 7, width = 7)
+plot(xgrid, rowSums(bumps), ylab = expression(hat(f)(x)),
+     type = "l", xlab = "x", lwd = 2, col = "red", 
+     main = "")
+out <- apply(bumps, 2, function(b) lines(xgrid, b, col = "blue"))
+dev.off()
