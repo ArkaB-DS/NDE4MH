71fb807ff9764e6f23f5ba63b1c909158b44e8ed
Nitin Garg <nitin.garg1000@gmail.com>
2022-04-15 15:15:43 +0530

    Update motivator.R

--- R Codes/motivator.R
+++ R Codes/motivator.R
@@ -1,5 +1,5 @@
-## Example for bump-killing
-# target: N(0, 1)
+## Motivator for bump-killing
+# target: Gamma(3, 1)
 # proposal: N(x, 100)
 
 # h <- 0.5
@@ -8,34 +8,34 @@ fact <- 1/sqrt(2*pi)
 
 expo <- function(x) exp(-(x^2)/2)*fact
 
+n <- 1e5
+
 one_sample <- function(len = 1e5)
 {
-  x <- numeric(len) + 2
-  prob <- numeric(len)
+  x <- rep(3, len)
   for (i in 2:len)
   {
-    #y <-rnorm(1, x[i-1] - h/2*eval(deriv(~log(dnorm(x)), "x[i-1]")), sqrt(h)) 
-    y <- rnorm(1, 100, 100)
-    if(runif(1) < dnorm(y)/dnorm(x[i-1]))
+    y <- rgamma(1, 3, 1.7)
+    if(runif(1) < dgamma(y, 3, 1)*dgamma(x[i-1], 3, 1.7)/
+       dgamma(x[i-1], 3, 1)/dgamma(y, 3, 1.7))
     {
       x[i] <- y
-      prob[i]<-1
     }else{
       x[i] <- x[i-1]
     }
   }
-  return(list(mc=x, acc.prob = mean(prob)))
+  return(x)
 }
 
 x <- one_sample()
-pdf("bk_trace.pdf", width = 7, height = 7)
-ts.plot(x$mc)
+pdf("bk_trace_motivator.pdf", width = 7, height = 7)
+ts.plot(x)
 dev.off()
-pdf("bk_density_plots.pdf", height = 7, width =7)
-plot(density(x$mc), ylab = expression(hat(f[h])), xlab = "",
+pdf("bk_density_plots_motivator.pdf", height = 7, width =7)
+plot(density(x), ylab = expression(hat(f[h])), xlab = "",
      main = "", lty = 2, ylim = c(0, 0.45), lwd = 2)
-lines(density(rnorm(1e5)), col = "red")
-h_iid <- density(x$mc)$bw
+lines(density(rgamma(1e5, 3, 1)), col = "red")
+h_iid <- density(x)$bw
 A.hat <- function(x)
 {
   n<-length(x)
@@ -52,9 +52,9 @@ A.hat <- function(x)
   
   return(sum(2*T - 1)/n)
 }
-A <- A.hat(x$mc)
+A <- A.hat(x)
 h_mh <- A^(1/5)*h_iid
-lines(density(x$mc, bw = h_mh), col = "blue", lty = 3)
+lines(density(x, bw = h_mh), col = "blue", lty = 3)
 
 Tis <- function(x)
 {
@@ -73,11 +73,11 @@ Tis <- function(x)
   return(2*T-1)
 }
 
-A_bk <- Tis(x$mc)
+A_bk <- Tis(x)
 h.bk <-  A_bk^(1/5)*h_iid
 M <- 1e3
 
-grid <- seq(from = -3, to = 3, length.out = M)
+grid <- seq(from = 0, to = 12, length.out = M)
 vals <- numeric(length = M)
 
 for(i in 1:M)
@@ -87,7 +87,7 @@ for(i in 1:M)
   sum <- 0
   for(j in 1:n)
   {
-    sum <- sum + expo((x$mc[j] - u)/h.bk[j])/h.bk[j]
+    sum <- sum + expo((x[j] - u)/h.bk[j])/h.bk[j]
   }
   vals[i] <- sum/n
 }
@@ -97,7 +97,7 @@ lines(grid, vals, type = "l", col = "green", lty = 4)
 legend("topright", legend = c("true density", "KDE(h_iid)",
                               "KDE(h_mh)", "KDE(h_bk)"),
        lty = c(1,2,3,4), col = c("red", "black", "blue", "green"),
-       cex = 0.8, lwd = c(1,2,1,1))
+       cex = 0.8, lwd = c(1,2,1,2))
 
 
 dev.off()
